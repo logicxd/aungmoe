5692ee702c657ec2ad7a6f91072c114fdb08730f
David Moe <aungmoe12@yahoo.com>
2018-04-03 04:38:43 -0700

    Initialized project

---
+++ index.js
@@ -0,0 +1,32 @@
+const PORT = process.env.PORT || 8081;
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var logger = require('morgan');
+
+// Server
+var server = app.listen(PORT, function () {
+    console.log('Server has started on port ' + PORT);
+});
+
+// View engine setup
+app.set('view engine', 'ejs');
+
+// Create controllers
+var index = require('./controllers/index');
+
+// Connect controllers 
+app.use(logger(':method :url :status :res[content-length] - :response-time ms'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', index);
+
+// Error handler
+app.use(function (err, req, res, next) {
+    // Render the error page
+    console.error(err.stack);
+    res.status(err.status || 500);
+    res.render('error', { error: err});
+});
+
+module.exports = app;
