83c2fcb27a9286a340b520bad3b7563682cc3b67
David Moe <aungmoe12@yahoo.com>
2018-04-08 04:30:13 -0700

    Header and footer looks good. Main page is still being worked on.

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 router.get('/', function (req, res) {
     res.render('index', { 
-        title: 'David Moe',
+        title: 'Aung Moe',
         description: 'Home Page',
         css: [global.css.material_icons, 'css/index.css'],
-        js: [global.js.jquery, 'js/_header.js']
+        js: [global.js.jquery, 'js/_header.js', 'js/index.js']
     });
 });
 
