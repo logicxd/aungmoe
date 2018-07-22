1b361e56b27d59ede1a108bb3a9523f38b6ebea6
David Moe <logicxd@gmail.com>
2018-07-22 00:36:19 -0700

    Resume added. Message changed. Job title added.

--- controllers/index.js
+++ controllers/index.js
@@ -4,14 +4,14 @@ var router = express.Router();
 router.get('/', function (req, res) {
     res.render('index', { 
         title: 'Aung Moe',
-        description: 'Learn more about Aung from his personal website!',
+        description: 'Learn more about Aung!',
         css: [global.css.material_icons, 'css/index.css', global.css.animate_css],
         js: [global.js.jquery, 'js/_header.js', 'js/index.js'],
         occupations: [
-            { state: "I'm a", occupation: "Software Engineer" },
-            { state: "Currently learning", occupation: "React + Nodejs" },
-            { state: "Just graduated from", occupation: "UCI with a BS" },
-            { state: "I'm available", occupation: "for hire" },
+            { state: "Experienced in", occupation: "full-stack development" },
+            { state: "Contributes to", occupation: "open source projects" },
+            { state: "Partook in", occupation: "researches at UCI" },
+            { state: "Graduated with", occupation: "honors in Computer Science" },
         ]
     });
 });
