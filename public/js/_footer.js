a2be77653fb1a05c72b53899f885c34bb85d035a
David Moe <logicxd@gmail.com>
2018-08-09 05:04:46 -0700

    Fixed navbar hamburger menu position

--- public/js/_footer.js
+++ public/js/_footer.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
     // M.AutoInit();
     $('.materialboxed').materialbox();
-    $('.sidenav').sidenav();
 });
---
+++ public/js/_header.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    // M.AutoInit();
+    $('.materialboxed').materialbox();
+    $('.sidenav').sidenav();
+});
