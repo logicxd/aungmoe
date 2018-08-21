3d203739fdeabb615a5104251a61ec122fab4eab
David Moe <logicxd@gmail.com>
2018-08-21 02:04:13 -0700

    Reverted back to not using a sticky navbar

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,33 @@ $(document).ready(function() {
       });
 });
 
+var animationEnd = (function (el) {
+    var animations = {
+        animation: 'animationend',
+        OAnimation: 'oAnimationEnd',
+        MozAnimation: 'mozAnimationEnd',
+        WebkitAnimation: 'webkitAnimationEnd',
+    };
+
+    for (var t in animations) {
+        if (el.style[t] !== undefined) {
+            return animations[t];
+        }
+    }
+    return 'animationend';
+})(document.createElement('div'));
+
+$.fn.extend({
+    animateCss: function (animationName, callback) {
+        this.addClass('animated ' + animationName).one(animationEnd, function () {
+            $(this).removeClass('animated ' + animationName);
+            if (typeof callback === 'function') callback();
+        });
+
+        return this;
+    },
+});
+
 var idNumber = 0;
 $('.occupations').on(animationEnd, function (e) {
     if (e.originalEvent.animationName === 'expandUnderlineToFull') {
