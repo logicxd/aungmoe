e395f4ddd39e0bf1ce0d604756187ee82fc05bf7
David Moe <logicxd@gmail.com>
2018-08-21 01:47:57 -0700

    Sticky navbar hides on scroll - test.

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,3 @@
-var idNumber = 0;
-var animationEnd = (function (el) {
-    var animations = {
-        animation: 'animationend',
-        OAnimation: 'oAnimationEnd',
-        MozAnimation: 'mozAnimationEnd',
-        WebkitAnimation: 'webkitAnimationEnd',
-    };
-
-    for (var t in animations) {
-        if (el.style[t] !== undefined) {
-            return animations[t];
-        }
-    }
-    return 'animationend';
-})(document.createElement('div'));
-
 $(document).ready(function() {
     $('#states-' + idNumber).css('display', 'inline-block');
     $('#occupations-' + idNumber).css('display', 'inline-block');
@@ -25,18 +8,7 @@ $(document).ready(function() {
       });
 });
 
-
-$.fn.extend({
-    animateCss: function (animationName, callback) {
-        this.addClass('animated ' + animationName).one(animationEnd, function () {
-            $(this).removeClass('animated ' + animationName);
-            if (typeof callback === 'function') callback();
-        });
-
-        return this;
-    },
-});
-
+var idNumber = 0;
 $('.occupations').on(animationEnd, function (e) {
     if (e.originalEvent.animationName === 'expandUnderlineToFull') {
         var currentState = $('#states-' + idNumber);
@@ -50,7 +22,6 @@ $('.occupations').on(animationEnd, function (e) {
             nextOccupation = $('#occupations-' + idNumber);
             nextState = $('#states-' + idNumber);
         }
-
 
         currentOccupation.animateCss('bounceOutDown', function () {
             nextOccupation.css('display', 'inline-block').animateCss('bounceInDown');
