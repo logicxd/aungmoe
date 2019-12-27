eb1dfc7a4180676b3ca5fb9ffa4a5a4c0f8c8386
Aung David Moe <aungmoe12@yahoo.com>
2019-12-27 00:42:07 -0800

    Added tap to scroll. Added home button.

--- public/js/read.js
+++ public/js/read.js
@@ -1,5 +1,19 @@
+var height = document.documentElement.clientHeight;
+
 $('#submit').click(() => {
     var url = $('#url').val();
     var autoloadNext = $('#autoload-next').is(':checked');
-    window.location.href = `?url=${url}&autoloadNext=${autoloadNext}`;
+    setCookie('autoloadNext', autoloadNext);
+    window.location.href = `?autoloadNext=${autoloadNext}&url=${url}`;
 });
+
+$('.tap-to-scroll').click(() => {
+    var height = document.documentElement.clientHeight;
+    var scrollByHeight = height * 0.90;
+    window.scrollBy(0, scrollByHeight);
+});
+
+function autoscroll() {
+    var element = document.getElementById("text-paragraph-10");
+    element.scrollIntoView({block: "center"});
+}
