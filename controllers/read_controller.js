6edcf6d702214541e452e54ff3f15a7b8b79b1ac
David Moe <aungmoe12@yahoo.com>
2020-04-19 22:41:20 -0700

    Better title searching and better alternative title searching.

--- controllers/read_controller.js
+++ controllers/read_controller.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var rp = require('request-promise');
 var unfluff = require('unfluff');
 var cheerio = require('cheerio');
+var loadedCheerio = null;
 
 router.get('/', async function (req, res) {
     if (req.query.url) {
@@ -24,6 +25,7 @@ function loadSetupPage(req, res) {
 
 async function loadReadPage(req, res) {
     var html = '';
+    loadedCheerio = null;
     try {
         html = await rp(req.query.url);
     } catch (error) {
@@ -32,23 +34,76 @@ async function loadReadPage(req, res) {
     }
     
     var data = unfluff(html);
+    var textTitles = findTextTitle(data.title, html);
     var paragraphs = data.text.split('\n\n');
-    var title = data.title || 'Error';
     var nextPageLink = findNextPageLinkUsingUnfluff(data.links);
     nextPageLink = nextPageLink === '' ? findNextPageLinkUsingCheerio(html) : nextPageLink;
     nextPageLink = turnURLIntoAbsolutePathIfNeeded(nextPageLink, req.query.url);
 
     res.render('read', {
-        title: `${title} - Aung Moe`,
+        title: `${data.title || 'Unknown'} - Aung Moe`,
         description: 'Aung\'s personal website',
         css: [global.css.material_icons, '/css/read.css', global.css.animate_css, global.css.fontawesome],
         js: [global.js.jquery, global.js.materialize, global.js.header, '/js/read.js', global.js.footer],
-        textTitle: title,
+        textTitle: textTitles[0],
+        textAlternativeTitles: getAlternativeTitleString(textTitles),
         textParagraphs: paragraphs,
         didError: html === '',
         currentPageLink: req.query.url,
         nextPageLink: nextPageLink
     });
+}
+
+// returns an array of ['Text Title', 'Alt. title 1', 'Alt. Title 2', ...]
+function findTextTitle(unfluffTitle, html) {
+    loadedCheerio = loadedCheerio == null ? cheerio.load(html) : loadedCheerio;
+    var titleCandidates = [unfluffTitle];
+    var selectors = ['h1', 'h2', 'h3', 'p:contains("Chapter")'];
+    var textTitles = [unfluffTitle];
+    var subTitleSet = new Set([unfluffTitle]); 
+    try {
+        // Grab possible content titles from `selectors` elements
+        for (var i = 0; i < selectors.length; ++i) {
+            loadedCheerio(selectors[i]).contents().each(function(i, node) {
+                if (node.data != null) {
+                    var title = node.data.trim();
+                    if (title.length > 0) {
+                        titleCandidates.push(title);
+
+                        // 'h1' selectors are added as alternative title
+                        if (selectors[i] === 'h1') {
+                            subTitleSet.add(title);
+                        }
+                    }
+                }
+            });
+        }
+        // Determine text title
+        titleCandidates.forEach(title => {
+            var useTitle = title.length >= textTitles[0].length;
+            useTitle &= title.toLowerCase().includes('chapter');
+            if (useTitle) {
+                textTitles[0] = title;
+            }
+        });
+        subTitleSet.delete(textTitles[0]);
+        subTitleSet.forEach(subTitle => {
+            textTitles.push(subTitle);
+        });
+    } catch (error) {
+        console.error(error);
+        textTitles = [unfluffTitle];
+    }
+    return textTitles;
+}
+
+function getAlternativeTitleString(textTitles) {
+    var titles = textTitles.slice(1);
+    var titleString = '';
+    for (var i = 0; i < titles.length && i < 5; ++i) {
+        titleString += `${i>0 ? ',' : ''} ${titles[i]}`;
+    }
+    return titleString;
 }
 
 function findNextPageLinkUsingUnfluff(links) {
@@ -65,16 +120,16 @@ function findNextPageLinkUsingUnfluff(links) {
 }
 
 function findNextPageLinkUsingCheerio(html) {
+    loadedCheerio = loadedCheerio == null ? cheerio.load(html) : loadedCheerio;
+    var linkContents = loadedCheerio('a').contents();
     var link = '';
-    var loadedHTML = cheerio.load(html);
-    var linkContents = loadedHTML('a').contents();
     try {
         for (var i = linkContents.length - 1; i >= 0; --i) {
-            var nodeObject = linkContents.get(i);
-            var nodeText = nodeObject.data;
+            var node = linkContents.get(i);
+            var nodeText = node.data;
             nodeText = nodeText == null ? '' : nodeText.toLowerCase();
             if (nodeText.includes('next')) {
-                link = nodeObject.parent.attribs.href;
+                link = node.parent.attribs.href;
                 break;
             }
         }
