923bb023a7809f4783ad22ca9ba2d8d545f3b3c9
David Moe <logicxd@gmail.com>
2021-01-31 20:20:26 -0800

    Remove newrelic. I don't have a use for it

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 "use strict";
 const PORT = process.env.PORT || 8081;
 
-require('newrelic');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
@@ -82,7 +81,6 @@ global.js = {
     materialize: '/js/materialize.min.js',
     header: '/js/_header.js',
     footer: '/js/_footer.js',
-    highlight: '/scripts/highlight.js',
     highlightCDN: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.4.0/languages/go.min.js'
 }
 global.configs = {
