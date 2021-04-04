1494bfc8408640791bff94c9198267d1390da81b
David Moe <logicxd@gmail.com>
2021-04-04 07:44:23 -0700

    Add projects page

--- src/app-area/project/project-controller.js
+++ src/app-area/project/project-controller.js
@@ -8,14 +8,48 @@ var utility = require('../utility')
 /* #endregion */
 
 var route = 'project'
-utility.setupRouterPaths(router, __dirname, route)
+utility.setupRouterPaths(router, __dirname)
 
 router.get('/', async function (req, res) {
     return res.render(path.join(__dirname, 'view/project'), {
         title: 'Project - Aung Moe',
         description: 'List of projects',
-        css: [`${route}/css/read.css`]
+        css: [`${route}/css/project.css`],
+        projects: listOfProjects()
     })
 })
+
+function listOfProjects() {
+    return [{
+        title: 'Combine Email Bills Using Gmail API',
+        url: 'https://github.com/logicxd/Combine-Email-Bills-Using-Gmail-API',
+        dateString: '2021',
+        category: 'Node.js, Gmail, Nodemailer, Job Scheduler'
+        // github: 'asdf',
+        // image: 'https://www.logolynx.com/images/logolynx/6d/6d7a4ee07338032263b8d6c8679f13ae.jpeg',
+        // year: '2021',
+        // tags: ['gmail, server, job scheduler'] // todo
+    }, {
+        title: 'Webtoon Reading Tool',
+        url: '/read-webtoon',
+        dateString: '2021',
+        category: 'Scripting'
+    }, {
+        title: 'Novel Reading Tool',
+        url: '/read-novel',
+        dateString: '2019',
+        category: 'Scripting'
+    }, {
+        title: 'Make a Blog with Markdown Using NodeJS',
+        url: 'https://github.com/logicxd/blog-nodejs',
+        dateString: '2018',
+        category: 'Blog, Markdown, Syntax Highlighting'
+    }, {
+        title: 'My Personal Blog',
+        url: '/blog',
+        dateString: '2018',
+        category: 'Blog'
+    }]
+}
 
 module.exports = router
